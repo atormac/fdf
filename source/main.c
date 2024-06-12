f023cc6df5e7c23f9a8b59821a3835d178ed9fb3
atormac <atorma@proton.me>
2024-06-13 00:28:42 +0300

    render box

--- source/main.c
+++ source/main.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 1024
+#define HEIGHT 1024
 
 static void ft_error(void)
 {
@@ -20,6 +20,8 @@ static void ft_hook(void* param)
 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
 
+static mlx_image_t * img;
+
 static void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
 	mlx_t* mlx;
@@ -28,20 +30,32 @@ static void	my_keyhook(mlx_key_data_t keydata, void *param)
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 	{
 		mlx_close_window(mlx);
+		return ;
 	}
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		img->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		img->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		img->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		img->instances[0].x += 5;
+
 }
 
 int main(void)
 {
-	mlx_set_setting(MLX_MAXIMIZED, true);
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
 	if (!mlx)
 		ft_error();
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	img = mlx_new_image(mlx, 256, 256);
+	ft_memset(img->pixels, 255, img->width * img->height * sizeof(int));
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
 
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+	//mlx_put_pixel(img, 0, 0, 0xFF0000FF);
 
 	mlx_key_hook(mlx, &my_keyhook, mlx);
 	mlx_loop_hook(mlx, ft_hook, mlx);
